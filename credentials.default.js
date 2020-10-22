921d05d5e8e3e6f7bc12a74024496c85245150c7
William Kuo <william.k.kuo@gmail.com>
2020-10-21 21:20:20 -0400

    Add default credentials file

---
+++ credentials.default.js
@@ -0,0 +1,15 @@
+export const FIREBASE_CONFIG_CREDENTIALS = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+export const GOOGLE_CONFIG_CREDENTIALS = {
+  expoClientId: "CLIENT_ID",
+  androidClientId: "CLIENT_ID",
+  androidStandaloneAppClientId: "STANDALONE_CLIENT_ID",
+};
